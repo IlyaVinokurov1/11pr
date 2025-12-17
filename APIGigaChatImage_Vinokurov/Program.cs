3a4388c35cb1c538b6b54819c83b0389d37b228c
Ilya <ilya.vinokurov.0006@gmail.com>
2025-12-17 21:08:42 +0500

    Разработанный метод для получения токена пользователя. Vinokurov

--- APIGigaChatImage_Vinokurov/Program.cs
+++ APIGigaChatImage_Vinokurov/Program.cs
@@ -1,19 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APIGigaChatImage_Vinokurov
 {
-    //static string ClientId = "019b287d-4c6f-7695-97bd-095b75ac26a5";
-    //static string AuthorizationKey = "MDE5YjI4N2QtNGM2Zi03Njk1LTk3YmQtMDk1Yjc1YWMyNmE1OmY5M2M4N2Q0LTJkNTgtNGIwNC05NmMxLTI0YzljNWMzOTM5Yw==";
+   
     internal class Program
     {
-
+        static string ClientId = "019b287d-4c6f-7695-97bd-095b75ac26a5";
+        static string AuthorizationKey = "MDE5YjI4N2QtNGM2Zi03Njk1LTk3YmQtMDk1Yjc1YWMyNmE1OmY5M2M4N2Q0LTJkNTgtNGIwNC05NmMxLTI0YzljNWMzOTM5Yw==";
         static void Main(string[] args)
         {
 
+        }
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string ReturnToken = null;
+            string Url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyError) => true;
+                using (HttpClient client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer {bearer}");
+                    var Data = new List<KeyValuePair<string, string>>
+                    {
+                       new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+                    };
+                    Request.Content = new FormUrlEncodedContent(Data);
+                    HttpResponseMessage Response = await client.SendAsync(Request);
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
+            return ReturnToken;
         }
     }
 }
